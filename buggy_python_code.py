59d80bed3ae947751391f95bb4c47c786ad25ea6
Matox Svk <75246510+MatoXsvk@users.noreply.github.com>
2026-05-07 12:59:33 +0200

    Fix urllib import handling in fetch_website function
    Handle urllib import for both urllib3 and urllib2.

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -31,6 +31,10 @@ def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
+    try:
+        import urllib3 as urllib
+    except:
+        import urllib2 as urllib
 
     try:
         http = urllib.PoolManager()
